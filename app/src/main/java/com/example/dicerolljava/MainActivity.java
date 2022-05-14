29ada0d030378c5a849a82915d8b8eb968bffac0
rahn <ryanahn2000@gmail.com>
2022-05-14 12:00:35 -0700

    Updates

--- app/src/main/java/com/example/dicerolljava/MainActivity.java
+++ app/src/main/java/com/example/dicerolljava/MainActivity.java
@@ -1,14 +1,8 @@
 package com.example.dicerolljava;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.TextView;
@@ -18,9 +12,6 @@ public class MainActivity extends AppCompatActivity {
     TextView number;
     NumberPicker numberPicker1;
     NumberPicker numberPicker2;
-    int tensDigit;
-    int onesDigit;
-    int entireNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
